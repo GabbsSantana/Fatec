71b0d53586210f9a5891bde38968b6e1e14bba03
itsmorais <michael_morais@outlook.com.br>
2022-09-18 17:55:51 -0300

    Código comentado

--- app.py
+++ app.py
@@ -2,28 +2,33 @@ from flask import Flask,render_template,request,flash,redirect,url_for
 
 app = Flask(__name__)
   
-
+# Criando classe para captura de dados do usuário e criação do chamad
+# Número do laboratório, número do micro, Problema relatado
 class Chamados:
     def __init__(self, laboratorio='', micro='', problema=''):
         self.laboratorio = laboratorio
         self.micro = micro
         self.problema = problema
 
+# Classe responsável pela captura de dados do usuário (nome e email)
 class alunos:
     def __init__(self, nome='', email=''):
         self.nome = nome
         self.email = email
        
-
+# Inicializando objetos
 chamado = Chamados
 aluno = alunos
+# Inicializando lista 
 chamados_abertos = []
 
-
+# Rota para página home
 @app.route('/')
 def home():
   return render_template('home.html')
 
+# Rota para página SELECT
+# Rota responsável por receber do usuário dados referente ao laboratório
 @app.route('/select/',methods=(['GET','POST']))
 def select():
   if request.method == 'POST':
@@ -37,6 +42,10 @@ def select():
   return render_template('select.html')
   
 
+# Rota para para página Layout
+# Rota responsável por apresentar o layout do laboratório escolhido
+# Rota responsável por receber dados referente ao micro e o problema reportado
+# Rota responsável por receber nome e email do usuário
 @app.route('/layout/',methods=(['GET','POST']))
 def layout():
   if request.method == 'POST':
@@ -57,12 +66,15 @@ def layout():
 
   return render_template('layout.html',chamado=chamado,aluno=aluno,chamados_abertos=chamados_abertos)
 
-
+# Rota para página Done
+# Rota responsável por apresentar ao usuário a conclusão do chamado e os dados sobre
 @app.route('/done/')
 def done(): 
   chamado.registrado = True
   return render_template('done.html',chamado=chamado,aluno=aluno)
 
+# Rota para página all
+# Rota responsável por apresentar ao usuário todos os chamados abertos no sistema
 @app.route('/all/')
 def all():
   return render_template('all.html',chamados=chamados_abertos)
